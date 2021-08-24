169ca2da13492bd3f820384f45f44375323f5c0b
sachintha00 <sachintha0603@gmail.com>
2021-08-24 19:00:49 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listView = findViewById(R.id.listViewSong);
         runtimePermission();
     }
 
